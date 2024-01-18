02509a960949e39b34e4ed9df1d38ce85a0b6722
sheeiavellie <josephlandmars@gmail.com>
2024-01-18 19:46:23 +0300

    add basic methods

--- lua/magic8ball/init.lua
+++ lua/magic8ball/init.lua
@@ -1,1 +1,30 @@
-print("🗡️")
+--@class Magic8Ball
+--@field config Magic8BallConfig
+--@field ui Magic8BallUI
+local Magic8Ball = {}
+
+Magic8Ball.__index = Magic8Ball
+
+--@return Magic8Ball
+function Magic8Ball:new()
+    local config = "amongus" --TODO:add config here
+
+    local magic8Ball = setmetatable({
+        config = config,
+        ui = "amogus" --TODO:add ui
+    }, self)
+
+    return magic8Ball
+end
+
+local magic8Ball = Magic8Ball:new()
+
+--@param self Magic8Ball
+--@param config Magic8BallConfig
+function Magic8Ball.setup(self, config)
+    --TODO:do config magic here
+
+    return self
+end
+
+return magic8Ball
