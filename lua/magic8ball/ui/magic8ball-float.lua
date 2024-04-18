25ce980312db9d2ad1a7143767076c5b8add5262
sheeiavellie <josephlandmars@gmail.com>
2024-04-19 01:35:17 +0300

    fix: achive wanted behavior from :quit float fix: fix naming

--- lua/magic8ball/ui/magic8ball-float.lua
+++ lua/magic8ball/ui/magic8ball-float.lua
@@ -5,8 +5,8 @@ local utils = require("magic8ball.utils")
 --@field buf_id number
 --@field win_id number
 --@field closing boolean
-local Magic8ballFloat = {}
-Magic8ballFloat.__index = Magic8ballFloat
+local Magic8BallFloat = {}
+Magic8BallFloat.__index = Magic8BallFloat
 
 local function close_window(buf_id, win_id)
     if win_id ~= nil and vim.api.nvim_win_is_valid(win_id) then
@@ -39,24 +39,24 @@ local function create_window()
     local buf_id = vim.api.nvim_create_buf(false, true)
 
     vim.api.nvim_buf_set_lines(buf_id, 4, 4, false, {
-        "AMOGUS Press ENTER to get the answer...",
+        "Press X to get the answer...",
     })
     local config = create_window_config()
     local win_id = vim.api.nvim_open_win(buf_id, true, config)
     return buf_id, win_id
 end
 
-function Magic8ballFloat.new(m8b_state)
+function Magic8BallFloat.new(m8b_state)
     local self = setmetatable({
         m8b_state = m8b_state,
         buf_id = nil,
         win_id = nil,
         closing = false,
-    }, Magic8ballFloat)
+    }, Magic8BallFloat)
     return self
 end
 
-function Magic8ballFloat:resize()
+function Magic8BallFloat:resize()
     if self.win_id == nil then
         return
     end
@@ -64,7 +64,7 @@ function Magic8ballFloat:resize()
     vim.api.nvim_win_set_config(self.win_id, config)
 end
 
-function Magic8ballFloat:toggle()
+function Magic8BallFloat:toggle()
     if self.buf_id == nil then
         local buf_id, win_id = create_window()
         self.buf_id = buf_id
@@ -75,7 +75,8 @@ function Magic8ballFloat:toggle()
                 return
             end
 
-            close_window(nil, self.win_id)
+            -- This thing desintegrates buffer when :quit
+            close_window(self.buf_id, self.win_id)
             self.buf_id = nil
             self.win_id = nil
         end)
@@ -88,4 +89,4 @@ function Magic8ballFloat:toggle()
     end
 end
 
-return Magic8ballFloat
+return Magic8BallFloat
--- lua/magic8ball/utils.lua
+++ lua/magic8ball/utils.lua
@@ -3,7 +3,7 @@ local M = {}
 M.magic8ball_group_id = vim.api.nvim_create_augroup("Magic8Ball", {})
 
 function M.on_close(buf_id, cb)
-    vim.api.nvim_create_autocmd('BufUnload', {
+    vim.api.nvim_create_autocmd({ 'WinClosed', 'BufUnload' }, {
         group = M.magic8ball_group_id,
         buffer = buf_id,
         callback = function()
